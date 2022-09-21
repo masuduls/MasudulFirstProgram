9ab83323c0b9cb9cd149ec23cfefb01f3bbbcba4
1E10 Laptop <sample@bths.edu>
2022-09-21 10:38:04 -0400

    changed the print statement and added a scanner

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Hello, how are you? ");
+    }
 }
